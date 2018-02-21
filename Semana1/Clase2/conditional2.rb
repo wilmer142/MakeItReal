2f1a04534458f9e19535015727ef957a34aa3588
wilmer142 <wilmercollege@gmail.com>
2018-02-20 20:04:08 -0500

    Probando condicionales

---
+++ Semana1/Clase2/conditional2.rb
@@ -0,0 +1,13 @@
+num = 24
+
+if num >= 10
+	if num <= 20
+		"El numero se encuentra entre 10 y 20"
+	else
+		"El numero no es menor que 20"
+	end
+else
+	"El numero no es mayor que 10"
+end
+
+puts (num >= 10 && num <= 20) ? "El numero se encuentra entre 10 - 20" : "El numero no se encuentra en el ranog de 10 - 20"
